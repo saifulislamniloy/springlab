22d83d87b0d5b0466579605b3d7839156dd9a202
saifulislamniloy <software.engineer.niloy@gmail.com>
2025-09-14 12:43:29 +0600

    feat: add vault integration for basic post operation with postgres in a second service

---
+++ vault/hashicorp/central-secret-management/service2/src/main/java/io/github/saifulislamniloy/springlab/vault/hashicorp/central_secret_management/service2/Service2Application.java
@@ -0,0 +1,13 @@
+package io.github.saifulislamniloy.springlab.vault.hashicorp.central_secret_management.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service2Application.class, args);
+	}
+
+}
