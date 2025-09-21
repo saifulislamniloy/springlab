9bf7c1353c9584ce3b6584abb52d307a332c8f4e
saifulislamniloy <software.engineer.niloy@gmail.com>
2025-09-21 14:42:43 +0600

    feat: secret management security update

--- vault/hashicorp/central-secret-management/service2/src/main/java/io/github/saifulislamniloy/springlab/vault/hashicorp/central_secret_management/service2/Service2Application.java
+++ vault/hashicorp/central-secret-management/service2/src/main/java/io/github/saifulislamniloy/springlab/vault/hashicorp/central_secret_management/service2/Service2Application.java
@@ -1,13 +1,32 @@
 package io.github.saifulislamniloy.springlab.vault.hashicorp.central_secret_management.service2;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Service2Application {
+public class Service2Application  implements CommandLineRunner {
+    @Value("${SPRING_POSTGRES_HOST:__missing__}") String host;
+    @Value("${SPRING_POSTGRES_PORT:__missing__}") String port;
+    @Value("${SPRING_POSTGRES_DB:__missing__}")   String db;
+    @Value("${SPRING_POSTGRES_USERS:__missing__}") String user;
+    @Value("${SPRING_POSTGRES_PASSWORD:__missing__}") String pw;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(Service2Application.class, args);
 	}
+
+
+    @Override
+    public void run(String... args) {
+        System.out.println("=== Vault values ===");
+        System.out.println("HOST=" + host);
+        System.out.println("PORT=" + port);
+        System.out.println("DB=" + db);
+        System.out.println("USER=" + user);
+        System.out.println("PW.len=" + (pw == null ? 0 : pw.length()));
+        System.out.println("====================");
+    }
 
 }
