5bc29ee26ea09844985968e6edb14b254f33b236
saifulislamniloy <software.engineer.niloy@gmail.com>
2025-09-21 12:44:52 +0600

    feat(vault): update security in central management in service 1

--- vault/hashicorp/central-secret-management/service1/src/main/java/io/github/saifulislamniloy/springlab/vault/hashicorp/central_secret_management/service1/Service1Application.java
+++ vault/hashicorp/central-secret-management/service1/src/main/java/io/github/saifulislamniloy/springlab/vault/hashicorp/central_secret_management/service1/Service1Application.java
@@ -1,13 +1,31 @@
 package io.github.saifulislamniloy.springlab.vault.hashicorp.central_secret_management.service1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
-public class Service1Application {
+public class Service1Application implements CommandLineRunner {
+    @Value("${SPRING_POSTGRES_HOST:__missing__}") String host;
+    @Value("${SPRING_POSTGRES_PORT:__missing__}") String port;
+    @Value("${SPRING_POSTGRES_DB:__missing__}")   String db;
+    @Value("${SPRING_POSTGRES_USERS:__missing__}") String user;
+    @Value("${SPRING_POSTGRES_PASSWORD:__missing__}") String pw;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Service1Application.class, args);
 	}
+
+    @Override
+    public void run(String... args) {
+        System.out.println("=== Vault values ===");
+        System.out.println("HOST=" + host);
+        System.out.println("PORT=" + port);
+        System.out.println("DB=" + db);
+        System.out.println("USER=" + user);
+        System.out.println("PW.len=" + (pw == null ? 0 : pw.length()));
+        System.out.println("====================");
+    }
 
 }
